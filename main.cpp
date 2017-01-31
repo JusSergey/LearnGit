67dc1f23b3dfb7154ed1fc4259715bb995d9360f
Sergey Juice <jus.noname@gmail.com>
2017-01-31 16:01:56 +0200

    adding cycle for...

--- main.cpp
+++ main.cpp
@@ -2,8 +2,11 @@
 using std::cout;
 
 // it is a main
-int main(){
-    cout << "Hello, world...\n";
+int main() {
+    
+    for (int i = 0; i < 8; i++)
+        cout << "Hello, world...\n";
+    
     cout.flush();
     return 0;
 }
